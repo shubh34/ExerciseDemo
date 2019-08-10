e290d7c9c91676ca6e11d4f50b0d6d43e9d8be9a
Shubham Jain <shubhamjain.qwerty@gmail.com>
2019-08-10 12:35:41 +0100

    folder structure and redux setup

--- src/index.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import Test from './js/components/Test';
-
-describe('First React component test with Enzyme', () => {
-	it('renders without crashing', () => {
-		expect(shallow(<Test />)).toMatchSnapshot();
-	});
-});
--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,23 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin }= require("clean-webpack-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-    module: {
-      rules: [
-        {
-          test: /\.(js|jsx)$/,
-          exclude: /node_modules/,
-          use: {
-            loader: "babel-loader"
-          }
-        }
-      ],
-    }, 
-    plugins: [
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+				},
+			},
+		],
+	},
+	plugins: [
 		new CleanWebpackPlugin(),
 		new HtmlWebpackPlugin({
 			title: 'Rakuten Tv Demo',
 			template: './index.ejs',
 		}),
 	],
-  };
+};
