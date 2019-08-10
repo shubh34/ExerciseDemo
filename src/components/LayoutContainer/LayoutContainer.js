a76003856cb8ddd79454a4941f79ad9bd927e8e6
Shubham Jain <shubhamjain.qwerty@gmail.com>
2019-08-10 19:00:10 +0100

    Middleware Api Integration

--- src/components/LayoutContainer/LayoutContainer.js
+++ src/components/LayoutContainer/LayoutContainer.js
@@ -1,8 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Proptypes from 'prop-types';
+import { connect } from 'react-redux';
+import { getMovieListByCategory, getMovieTrailor, getMovieDetails } from '../../states/movieListing/action';
+import { movieCategeries } from '../../configs/config';
 
-const LayoutContainer = (props) => {
-	console.log(props);
-	return <div> Layout Container</div>;
+const mapDispatch = dispatch => ({
+	getMovieList: categoryName => dispatch(getMovieListByCategory(categoryName)),
+	getMovieTrailor: () => dispatch(getMovieTrailor()),
+	getMovieDetails: () => dispatch(getMovieDetails()),
+
+});
+class LayoutContainer extends Component {
+	componentDidMount() {
+		const { getMovieList, getMovieTrailor, getMovieDetails } = this.props;
+		Object.keys(movieCategeries).forEach((id) => {
+			getMovieList(movieCategeries[id]);
+		});
+		// getMovieTrailor();
+		// getMovieDetails();
+	}
+
+	render() {
+		return <div>Hello world </div>;
+	}
+}
+LayoutContainer.propTypes = {
+	getMovieList: Proptypes.func.isRequired,
+	getMovieTrailor: Proptypes.func.isRequired,
+	getMovieDetails: Proptypes.func.isRequired,
 };
-
-export default LayoutContainer;
+export default connect(null, mapDispatch)(LayoutContainer);
---
+++ src/configs/config.js
@@ -0,0 +1,9 @@
+export const movieCategeries = {
+	1: 'populares-en-taquilla',
+	2: 'estrenos-para-toda-la-familia',
+	3: 'estrenos-imprescindibles-en-taquilla',
+	4: 'estrenos-espanoles',
+	5: 'si-te-perdiste',
+	6: 'especial-x-men',
+	7: 'nuestras-preferidas-de-la-semana',
+};
