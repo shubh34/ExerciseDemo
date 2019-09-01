7cedf04aa20de0440146f3846bab817f6915849c
Shubham Jain <shubhamjain.qwerty@gmail.com>
2019-09-01 19:06:07 +0100

    Action test fix

--- src/states/movieListing/action.test.js
+++ src/states/movieListing/action.test.js
@@ -1,8 +1,23 @@
-import { fetchMovieListByCategory } from './action';
+import configureStore from 'redux-mock-store';
+import { apiMiddleware } from 'redux-api-middleware';
+import thunk from 'redux-thunk';
 
+import { fetchMovieListByCategory, createRSAAMovieListReuest } from './action';
+
+import { cacheApiResponse } from '../../utils/cacheApiUtils';
+
+jest.mock('../../utils/cacheApiUtils');
+
+const mockStore = configureStore([apiMiddleware, thunk]);
 
 describe('movie list action', () => {
 	it('should create action for getting movie list', () => {
-		expect(fetchMovieListByCategory('test')).toMatchSnapshot();
+		expect(createRSAAMovieListReuest('test')).toMatchSnapshot();
+	});
+	it('fetchTournamentPrizes should call api to fetch prizes', () => {
+		const store = mockStore();
+		return store.dispatch(fetchMovieListByCategory()).then(() => {
+			expect(cacheApiResponse).toBeCalled();
+		});
 	});
 });
